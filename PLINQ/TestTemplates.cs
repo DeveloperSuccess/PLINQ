83aaa866a2a89d66fdb678abe76035491e11e639
DeveloperSuccess <megafreez@gmail.com>
2023-06-15 10:42:46 +0500

    Убрал лишнее

--- PLINQ/TestTemplates.cs
+++ PLINQ/TestTemplates.cs
@@ -1,8 +1,4 @@
-﻿using System.Diagnostics;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
-
-namespace PLINQ
+﻿namespace PLINQ
 {
     internal class TestTemplates
     {
