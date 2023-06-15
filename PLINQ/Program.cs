c7a7038828b18ebfa39ac5daa383f59c4f241fbf
DeveloperSuccess <megafreez@gmail.com>
2023-06-15 10:49:14 +0500

    Вынес старт теста в шаблоны

--- PLINQ/Program.cs
+++ PLINQ/Program.cs
@@ -1,18 +1,3 @@
 ﻿using PLINQ;
 
-var test = new TestPLINQ();
-
-test.StartTest();
-
-Console.WriteLine($"Общее время выполнения теста: {test.ExecutionTime}");
-
-foreach (var result in test.Result.GetResultDefaultGrouping())
-{
-    Console.WriteLine($"Степень параллелизма: {result.DegreeParallelism}; " +
-    $"Элементов в коллекции: {result.RangeLimit}; Задержка: {result.Delay} ms; " +
-    $"Время последовательной обработки: {result.ExecutionTimeWithoutParallel}; " +
-    $"Время параллельной обработки: {result.ExecutionTimeWithParallel}; " +
-    $"Ускорение на: {result.Difference} ms");
-}
-
-Console.Read();
+TestTemplates.StartDefaultTest();
--- PLINQ/TestTemplates.cs
+++ PLINQ/TestTemplates.cs
@@ -3,9 +3,32 @@
     internal class TestTemplates
     {
         /// <summary>
+        /// Тестирование с авторскими значениями
+        /// </summary>
+        public static void StartDefaultTest()
+        {
+            var test = new TestPLINQ();
+
+            test.StartTest();
+
+            Console.WriteLine($"Общее время выполнения теста: {test.ExecutionTime}");
+
+            foreach (var result in test.Result.GetResultDefaultGrouping())
+            {
+                Console.WriteLine($"Степень параллелизма: {result.DegreeParallelism}; " +
+                $"Элементов в коллекции: {result.RangeLimit}; Задержка: {result.Delay} ms; " +
+                $"Время последовательной обработки: {result.ExecutionTimeWithoutParallel}; " +
+                $"Время параллельной обработки: {result.ExecutionTimeWithParallel}; " +
+                $"Ускорение на: {result.Difference} ms");
+            }
+
+            Console.Read();
+        }
+
+        /// <summary>
         /// Тест времени выполнения тестирования в зависимости от вложенности AsParallel
         /// </summary>
-        public void StartTestExecutionTimeNestingAsParallel()
+        public static void StartTestExecutionTimeNestingAsParallel()
         {
             var nestings = Enumerable.Range(0, 3);
 
@@ -15,9 +38,9 @@
         }
 
         /// <summary>
-        /// Тест времени выполнения тестирования в зависимости от вложенности AsParallel
+        /// Тестирование с пользовательскими значениями
         /// </summary>
-        public void ExampleCustomTest()
+        public static void StartExampleCustomTest()
         {
             var parallelismDegrees = new List<int>() { 2, 5, 512 };
 
